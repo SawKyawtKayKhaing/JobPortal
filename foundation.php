23c593dfce2a436e1469b9e25169b83d9e5a27cc
pyae-pyae-win <pyaepyaewin71@gmail.com>
2020-08-03 15:27:44 +0630

    foundation

---
+++ foundation.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+
+</style>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+<body style="background-color:orange;">
+    <a href ="jobSeeker.php"><button type="button" class="btn btn-outline-secondary">Job Seeker</button></a>
+    <a href ="jobProvider.php"><button type="button" class="btn btn-outline-secondary">Job Provider</button></a>
+</body>
+</html>
