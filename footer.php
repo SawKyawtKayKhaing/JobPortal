e5d1e8472469a09c259544c227a52fb78d130aa0
than-mar-htay <thanmarhtay87@gmail.com>
2020-08-05 22:00:41 +0630

    footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <title>Footer</title>
+    <style>
+        footer {
+        text-align: center;
+        background-color: DarkSalmon;
+        color: white;
+        }
+    </style>
+    <body>
+        <footer>
+            <p>About Job Seeker</p>
+        </footer>
+    </body>
+</html>
