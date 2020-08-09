0c04609b81d8f15851c777bbe7c56e4e830b3f3a
pyae-pyae-win <pyaepyaewin71@gmail.com>
2020-08-10 00:19:13 +0630

    pp

--- Job_Provider.php
+++ Job_Provider.php
@@ -109,8 +109,6 @@ include 'j-Provider.php';
             $myfile = fopen("j-Provider.txt", "a") or die("Unable to open file!");
             fwrite($myfile, $add."\n");
             fclose($myfile);
-
-<<<<<<< HEAD
             $servername ="localhost";
             $username = "root";
             $password = "";
@@ -121,11 +119,9 @@ include 'j-Provider.php';
                 die("Connection failed: " . $conn->connect_error);
             }
             $sql = "INSERT INTO  jobpost(job_name,salary,experience,age, gender,city,due_date) VALUES ('$name', '$salary','$experience','$age','$gender','$city','$duedate')";
-=======
             include 'database.php';
             
             $sql = "INSERT INTO  provider(cname,typeOfCompany, city,employee_qty, job_vacancy, experience,salary, age, gender,duedate) VALUES ('$name', '$companytype', '$city','$qty', '$jobvacancy','$experience','$salary','$age','$gender','$duedate')";
->>>>>>> 5a794c805fe0c4e50f8ad1e91bb4445dc9b64d75
 
                 if ($conn->query($sql) === TRUE) {
                     echo "New record created successfully";
--- foundation.php
+++ foundation.php
@@ -35,29 +35,26 @@ th, td {
 <?php
 include 'database.php';
 // Check connection
-<<<<<<< HEAD
 if ($conn->connect_error) {
   die("Connection Error");
 }
 $sql="SELECT seeker.name,provider.pname,seeker.position,applyjob.Apply_date,seeker.city,seeker.gender,provider.pduedate FROM seeker join applyjob join provider where seeker.id=applyjob.sid and applyjob.provider_id=provider.pid and seeker.position=provider.pjob_vacancy and seeker.experience=provider.pexperience and seeker.gender=provider.pgender;";
 echo "<table><thead><tr ><th>Seeker Id</th><th>Seeker Name</th><th>Company Name</th><th>Position</th><th>ApplyDate</th><th>City</th><th>Gender</th><th>Duedate</th></tr></thead><tbody>";
 $result = $conn->query($sql);
-=======
 
 
 $sql = "SELECT seeker.sid, seeker.name, company.cname,seeker.position,apply.Apply_date,seeker.city,seeker.gender,company.duedate FROM seeker join company join apply on seeker.sid=apply.seekid and company.pid=apply.cid";
 echo "<table><thead><tr ><th>Seeker Id</th><th>Seeker Name</th><th>Company Name</th><th>Position</th><th>Apply Date</th><th>City</th><th>Gender</th><th>Duedate</th></tr></thead><tbody>";
->>>>>>> 5a794c805fe0c4e50f8ad1e91bb4445dc9b64d75
+
 
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-<<<<<<< HEAD
     echo "<tr>"."<td>". $row["id"]. "</td>"."<td>". $row["name"]."</td>". "<td>".$row["pname"]."</td>"."<td>".$row["position"]."</td>"."<td>".$row["Apply_date"]."</td>"."<td>".$row["city"]."</td>"."<td>".$row["gender"]."</td>"."<td>".$row["pduedate"]."</td>"."</tr>"."<br>";
-=======
+
     echo "<tr>"."<td>". $row["sid"]. "</td>"."<td>". $row["name"]."</td>". "<td>".$row["cname"]."</td>"."<td>".$row["position"]."</td>"."<td>".$row["Apply_date"]."</td>"."<td>".$row["city"]."</td>"."<td>".$row["gender"]."</td>"."<td>".$row["duedate"]."</td>"."</tr>"."<br>";
->>>>>>> 5a794c805fe0c4e50f8ad1e91bb4445dc9b64d75
+
     
   
   }
